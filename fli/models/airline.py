879732133f60e2b7bc63b723acc976f37601283b
DZ <ansontan635@gmail.com>
2026-04-01 11:44:09 -0700

    address comment

--- fli/models/airline.py
+++ fli/models/airline.py
@@ -863,7 +863,7 @@ class Airline(Enum):
     ZD = "Ewa Air"
     ZE = "Eastar Jet"
     ZF = "Azur Air"
-    ZG = "ZIPAIR"
+    ZG = "Zipair"
     ZH = "Shenzhen Airlines"
     ZI = "Aigle Azur"
     ZJ = "Zambezi Airlines"
