e3f92ba967bebc31fcd5346e4946cae7fad1b8ee
Anson <ansontan635@gmail.com>
2026-04-01 13:54:15 -0700

    chore: Update `ZG` airline enum from “Groznyy Avia” to “Zipair” (#105)
    * fix: update ZG code to ZIPAIR
    
    * address comment

--- fli/models/airline.py
+++ fli/models/airline.py
@@ -863,7 +863,7 @@ class Airline(Enum):
     ZD = "Ewa Air"
     ZE = "Eastar Jet"
     ZF = "Azur Air"
-    ZG = "Groznyy Avia"
+    ZG = "Zipair"
     ZH = "Shenzhen Airlines"
     ZI = "Aigle Azur"
     ZJ = "Zambezi Airlines"
